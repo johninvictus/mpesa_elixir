711dec3189a7c39ba527eb2c1c209bc35d6fa912
johninvictus <invictusnitude@gmail.com>
2018-04-05 15:14:27 +0300

    Initial commit

---
+++ lib/mpesa_elixir.ex
@@ -0,0 +1,18 @@
+defmodule MpesaElixir do
+  @moduledoc """
+  Documentation for MpesaElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MpesaElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mpesa_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule MpesaElixirTest do
+  use ExUnit.Case
+  doctest MpesaElixir
+
+  test "greets the world" do
+    assert MpesaElixir.hello() == :world
+  end
+end
