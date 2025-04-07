f0d9655ce4d9798a140a77654573fb1f484dc02e
invictus <invictusnitude@gmail.com>
2025-04-08 02:45:19 +0300

    added application to mix

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule MpesaElixir.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :httpotion, :timex]
+      extra_applications: [:logger, :httpotion, :timex],
+      mod: {MpesaElixir.Application, []}
     ]
   end
 
