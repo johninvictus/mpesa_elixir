6a7f338e39ce12220aadac9a284eb205278f9646
invictus <invictusnitude@gmail.com>
2025-05-02 14:39:31 +0300

    added Qrcode mox

--- test/mpesa_elixir/qr_code_test.exs
+++ test/mpesa_elixir/qr_code_test.exs
@@ -2,7 +2,9 @@ defmodule MpesaElixir.QrCodeTest do
   @moduledoc false
   use ExUnit.Case, async: true
 
-  import Mock
+  import Mox
+
+  setup :verify_on_exit!
 
   # Let's add some test data
   @valid_request %MpesaElixir.QrCode.Request{
@@ -29,30 +31,25 @@ defmodule MpesaElixir.QrCodeTest do
 
   describe "generate/1" do
     test "successfully generates QR code" do
-      with_mock MpesaElixir.API, [:passthrough],
-        request: fn "/mpesa/qrcode/v1/generate", _options ->
-          {:ok, %Req.Response{status: 200, body: @success_response}}
-        end do
-        assert {:ok, @success_response} == MpesaElixir.QrCode.generate(@valid_request)
-      end
+      expect(MpesaElixir.APIMock, :request, fn _url, _opts ->
+        {:ok, %Req.Response{status: 200, body: @success_response}}
+      end)
+      assert {:ok, @success_response} == MpesaElixir.QrCode.generate(@valid_request)
     end
 
     test "handles error response" do
-      with_mock MpesaElixir.API, [:passthrough],
-        request: fn "/mpesa/qrcode/v1/generate", _options ->
-          {:ok, %Req.Response{status: 400, body: @error_response}}
-        end do
-        assert {:error, @error_response} == MpesaElixir.QrCode.generate(@valid_request)
-      end
+      expect(MpesaElixir.APIMock, :request, fn _url, _opts ->
+        {:ok, %Req.Response{status: 400, body: @error_response}}
+      end)
+      assert {:error, @error_response} == MpesaElixir.QrCode.generate(@valid_request)
     end
 
     test "handles network error" do
-      with_mock MpesaElixir.API, [:passthrough],
-        request: fn "/mpesa/qrcode/v1/generate", _options ->
-          {:error, %Req.TransportError{reason: :timeout}}
-        end do
-        assert {:error, :timeout} == MpesaElixir.QrCode.generate(@valid_request)
-      end
+      expect(MpesaElixir.APIMock, :request, fn _url, _opts ->
+        {:error, %Req.TransportError{reason: :timeout}}
+      end)
+
+      assert {:error, :timeout} == MpesaElixir.QrCode.generate(@valid_request)
     end
   end
 end
